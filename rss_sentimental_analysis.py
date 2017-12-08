30a37ed3dfde4751394608b1dd068631f9e95130
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-12-08 15:48:19 -0700

    rss datetime and max entries
    Added functionality to retrieve the max number of entries on a given feed. In addition, added datetime property for each title.

--- rss_sentimental_analysis.py
+++ rss_sentimental_analysis.py
@@ -5,12 +5,12 @@ from textblob import TextBlob
 
 class RssClient(object):
 
-	def __init__(self, link=rss_link):
+	def __init__(self, link):
 		'''
 		Creates a RssFeed object to mine News Titles
 		'''
 		try:
-			rssFeed = feedparser.parse(link)
+			self.rssFeed = feedparser.parse(link)
 		except Error as e:
 			raise Exception("Failed to access RSS: " + str(e))
 
@@ -19,7 +19,7 @@ class RssClient(object):
 		Uses textblob's sentiment method to classify sentiment
 		passed tweet
 		'''
-		analysis = TextBlob(self.clean_tweet(tweet))
+		analysis = TextBlob(title)
 		if analysis.sentiment.polarity > 0:
 			return 'positive'
 		elif analysis.sentiment.polarity == 0:
@@ -32,21 +32,19 @@ class RssClient(object):
 		Uses a query to search for titles
 		'''
 		titles = []
+
+		if count == 'max':
+			count = len(self.rssFeed['entries'])
+
 		try:
-			fetched_titles = [self.rssFeed['entries'][i]['title'] for i in range(0, count)]
-			for title in fetched_titles:
+			fetched_titles = [(self.rssFeed['entries'][i].title, self.rssFeed['entries'][i].published) for i in range(0, count)]
+			for title, date in fetched_titles:
 				# Dictionary that will hold title text and title sentiment
 				parsed_title = {}
-				parsed_title['text'] = tweet.text
-				parsed_title['sentiment'] = self.get_tweet_sentiment(tweet.text)
-
-
-				if tweet.retweet_count > 0:
-					# if a tweet has retweets, ensures that it is only appended once
-					if parsed_tweet not in tweets:
-						tweets.append(parsed_tweet)
-				else:
-					tweets.append(parsed_tweet)
-			return tweets
-		except tweepy.TweepError as e:
+				parsed_title['text'] = title
+				parsed_title['sentiment'] = self.get_rss_sentiment(title)
+				parsed_title['datetime'] = date
+				titles.append(parsed_title)
+			return titles
+		except Exception as e:
 			raise Exception("Error: " + str(e))
